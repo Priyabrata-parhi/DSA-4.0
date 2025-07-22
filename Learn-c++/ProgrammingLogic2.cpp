90a6c26f5021f52acee34e704d5b9790d46ce2d3
Priyabrata-parhi <priyabrata2609@gmail.com>
2025-07-22 06:44:41 +0530

    Day-16

---
+++ Learn-c++/ProgrammingLogic2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // int a = 10;
+  // int b = 5;
+
+  // int answer = a % b;
+  // cout << answer << endl;
+
+  int a = 50;
+  int b = 5;
+
+  //relational operaetors
+  //cout << (a == b) ;
+  //cout << (a != b);
+  //cout << (a > b); 
+  //cout << (a < b);
+  //cout << (a >= b);
+  cout << (a <= b);
+
+  return 0;
+}
