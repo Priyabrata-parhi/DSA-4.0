3d97bd570c35c450e0f4d2afc402b088a268cb68
Priyabrata-parhi <priyabrata2609@gmail.com>
2025-08-23 23:19:03 +0530

    code-added

---
+++ Array/Time & Space Complexity.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+  // Time And Space Complexity
+  // Amount oF time taken by an algorithm to run function of length of input
+  // Not Actual TIme
+  // It Define CPU time
+  // Use Cases:
+  // To make efficient Programs
+  // Ask By Interviewer After Every Solution You Give
+
+  // Space
+  // Amount Of Space Taken By Algo as Function Of Length of Input
+  // Big O Notation -> Upper Bound -> Worst Case
+  // Theta  θ -> Average Case
+  // Omega Ω -> Lower Case -> Best Case
+  // O(1) -> Constant Time
+  // O(n) -> Linear Time
+  // O(n^2) -> Quadratic Time
+  // O(2^n) -> Exponential Time
+  // O(n!) -> Factorial Time
+  // O(n^n) -> Polynomial Time
+  // O(log2n) -> Logarithm Time
+  // O(n^3) -> Cubic Time
+  // Nesting {
+  //   {
+  //   Multiplication
+  //   }
+  // }
+
+  // Sequence{
+  // {}
+  // {Add}
+  // }
+
+  // F(n) : 4n^4 + 3n^3 + 2n^2 + Logn + Nlogn + n + 1; -> O(n^4)
+
+  return 0;
+}
